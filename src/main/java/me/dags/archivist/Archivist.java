c58afb6a38d6a4f52e32921961bc1b9ffe9cb8a9
dags- <dags@dags.me>
2018-05-03 11:53:53 +0100

    - 7.1.0 - rename file if already exists before moving

--- src/main/java/me/dags/archivist/Archivist.java
+++ src/main/java/me/dags/archivist/Archivist.java
@@ -1,5 +1,15 @@
 package me.dags.archivist;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.StandardCopyOption;
+import java.time.Month;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.api.Sponge;
@@ -8,13 +18,6 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.scheduler.Task;
 
-import java.io.IOException;
-import java.nio.file.*;
-import java.time.Month;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * @author dags <dags@dags.me>
  */
@@ -22,7 +25,8 @@ import java.util.regex.Pattern;
 public class Archivist implements Runnable {
 
     private final PathMatcher filter = FileSystems.getDefault().getPathMatcher("glob:*.log.gz");
-    private final Pattern pattern = Pattern.compile("((\\d{4})-(\\d{2})-\\d{2})");
+    private final Pattern datePattern = Pattern.compile("((\\d{4})-(\\d{2})-\\d{2})");
+    private final Pattern namePattern = Pattern.compile("(.*?)(-(\\d+))?.log.gz");
     private final Logger logger = LoggerFactory.getLogger("Archivist");
     private final Path logs = Sponge.getGame().getGameDirectory().resolve("logs").toAbsolutePath();
 
@@ -42,26 +46,28 @@ public class Archivist implements Runnable {
     }
 
     private void archive(Path source){
-        Matcher matcher = pattern.matcher(source.getFileName().toString());
-        if (!matcher.find()) {
+        Matcher dateMatcher = datePattern.matcher(source.getFileName().toString());
+        if (!dateMatcher.find()) {
             logger.info("Skipping file {}", source);
             return;
         }
 
         // String date = matcher.group(1);
-        String year = matcher.group(2);
-        String month = matcher.group(3);
+        String year = dateMatcher.group(2);
+        String month = dateMatcher.group(3);
         String folder = String.format("%s-%s", month, Month.of(Integer.parseInt(month)));
 
         Path dir = logs.resolve(year).resolve(folder);
         Path destination = dir.resolve(source.getFileName());
 
         if (Files.exists(destination)) {
-            String file = destination.getFileName().toString();
-            String name = file.replace(".log.gz", "");
-            for (int i = 0; Files.exists(destination); i++) {
-                String fileName = String.format("%s-%s.log.gz", name, i);
-                destination = dir.resolve(fileName);
+            Matcher nameMatcher = namePattern.matcher(source.getFileName().toString());
+            if (nameMatcher.find()) {
+                String name = nameMatcher.group(1);
+                for (int i = 1; Files.exists(destination); i++) {
+                    String fileName = String.format("%s-%s.log.gz", name, i);
+                    destination = dir.resolve(fileName);
+                }
             }
         }
 
